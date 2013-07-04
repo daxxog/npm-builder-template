739650de62c6e3544f84aac9977ba2c5eb668e1a
David Volm <david@volminator.com>
2013-07-04 12:50:03 -0500

    smooth optimised publishing

---
+++ postpublish.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# {{Name}} / postpublish.sh
+# postpublish script for {{Name}}
+# (c) 2013 David (daXXog) Volm ><> + + + <><
+# Released under Apache License, Version 2.0:
+# http://www.apache.org/licenses/LICENSE-2.0.html  
+#################################################
+
+mv ../.tmp.js {{name}}.js
+mv ../.tmp.h {{name}}.h
