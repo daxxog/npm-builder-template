2d19029083890f450b85718290f8b40238556a05
David Volm <david@volminator.com>
2018-01-02 12:28:34 -0600

    happy new year!

--- y-name-x.h
+++ y-name-x.h
@@ -1,6 +1,6 @@
 /* {{Name}}
  * {{description}}
- * (c) 2016 David (daXXog) Volm ><> + + + <><
+ * (c) 2018 David (daXXog) Volm ><> + + + <><
  * Released under Apache License, Version 2.0:
  * http://www.apache.org/licenses/LICENSE-2.0.html  
  */ 
